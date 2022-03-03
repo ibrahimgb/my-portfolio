1032f3cff96f7e4c978172394d63f63bf98affa2
ibrahim gb <ibrahim.guoual.b@gmail.com>
2022-03-03 23:32:38 +0100

    first commit

---
+++ portfolio/src/components/header/Btns.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import CV from "../../assets/CV.pdf";
+
+const Btns = () => {
+    return (
+        <div className="btns">
+            <a href={CV} download className="btn">
+                tcheck my cv
+            </a>
+            <a href="#contact" className="btn btn-primary">
+                Let's talk
+            </a>
+        </div>
+    );
+};
+
+export default Btns;
