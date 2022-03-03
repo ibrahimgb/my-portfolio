9db30f3ca666f5b7e37ba19cacfdb968bc3226ea
ibrahim gb <ibrahim.guoual.b@gmail.com>
2022-03-03 23:40:43 +0100

    setup

---
+++ src/components/testimoniales/Testimoniales.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./testimoniales.css";
+
+const Testimoniales = () => {
+    return <section id="testimoniales">Testimoniales</section>;
+};
+
+export default Testimoniales;
