0e882aecc1e987fb949ecb8151da2efd3ca0cd28
ibrahim gb <ibrahim.guoual.b@gmail.com>
2022-03-04 23:27:48 +0100

    adding responsiveness to the components

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,7 +1,97 @@
-import React from "react";
 import "./contact.css";
+import { useRef } from "react";
+import emailjs from "@emailjs/browser";
+
+import { MdOutlineMail } from "react-icons/md";
+import { BsLinkedin } from "react-icons/bs";
+
 const Contact = () => {
-    return <section id="contact">Contact</section>;
+    const form = useRef();
+
+    const sendEmail = (e) => {
+        e.preventDefault();
+
+        emailjs
+            .sendForm(
+                "service_dgd7dy1",
+                "template_rth9yhl",
+                form.current,
+                "WbwaHXtafiI_VOIPd"
+            )
+            .then(
+                (result) => {
+                    console.log(result.text);
+                    e.target.reset();
+                },
+                (error) => {
+                    console.log(error.text);
+                }
+            );
+    };
+
+    return (
+        <section id="contact">
+            <h5>Get In Touch</h5>
+            <h2>Contact Me</h2>
+            <div className="container contact__container">
+                <div className="contact__options">
+                    <article className="contact__option">
+                        <MdOutlineMail className="icon" />
+                        <h4>Email</h4>
+                        <h5>ibrahim.guoual.b@gmail.com</h5>
+                        <a
+                            href="mailto:ibrahim.guoual.b@gmail.com"
+                            target="_blank"
+                        >
+                            {" "}
+                            Send a massage
+                        </a>
+                    </article>
+
+                    <article className="contact__option">
+                        <BsLinkedin className="icon" />
+                        <h4>Linkedin</h4>
+                        <h5>@ibrahimgb</h5>
+                        <a
+                            href="https://www.linkedin.com/in/ibrahimgb/"
+                            target="_blank"
+                        >
+                            Send a massage
+                        </a>
+                    </article>
+                </div>
+                <form ref={form} onSubmit={sendEmail}>
+                    <input
+                        type="text"
+                        name="name"
+                        placeholder="your name"
+                        required
+                    />
+                    <input
+                        type="email"
+                        name="email"
+                        placeholder="your email"
+                        required
+                    />
+                    <input
+                        type="text"
+                        name="subject"
+                        placeholder="your email"
+                        required
+                    />
+                    <textarea
+                        name="message"
+                        rows="7"
+                        placeholder="your message"
+                        required
+                    ></textarea>
+                    <button type="submit" className="btn btn-primary">
+                        Send Message
+                    </button>
+                </form>
+            </div>
+        </section>
+    );
 };
 
 export default Contact;
--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -5,21 +5,53 @@ import { BiBook, BiMessageSquareDetail } from "react-icons/bi";
 import { RiServiceLine } from "react-icons/ri";
 
 const Nav = () => {
+    const [activeNav, setActiveNav] = React.useState("#"); //className="active"
+
     return (
         <nav>
-            <a href="#about">
+            <a
+                href="#"
+                onClick={() => {
+                    setActiveNav(() => "#");
+                }}
+                className={activeNav === "#" ? "active" : ""}
+            >
                 <AiOutlineHome />
             </a>
-            <a href="#about">
+            <a
+                href="#about"
+                onClick={() => {
+                    setActiveNav(() => "#about");
+                }}
+                className={activeNav === "#about" ? "active" : ""}
+            >
                 <AiOutlineUser />
             </a>
-            <a href="#experience">
+            <a
+                href="#experience"
+                onClick={() => {
+                    setActiveNav(() => "#experience");
+                }}
+                className={activeNav === "#experience" ? "active" : ""}
+            >
                 <BiBook />
             </a>
-            <a href="#portfolio">
+            <a
+                href="#portfolio"
+                onClick={() => {
+                    setActiveNav(() => "#portfolio");
+                }}
+                className={activeNav === "#portfolio" ? "active" : ""}
+            >
                 <RiServiceLine />
             </a>
-            <a href="#contact">
+            <a
+                href="#contact"
+                onClick={() => {
+                    setActiveNav(() => "#contact");
+                }}
+                className={activeNav === "#contact" ? "active" : ""}
+            >
                 <BiMessageSquareDetail />
             </a>
         </nav>
