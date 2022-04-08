f3027641005824a3581b746644d674523b75401c
ibrahim gb <59414164+ibrahimgb@users.noreply.github.com>
2022-04-08 04:04:54 +0100

    Delete src/components directory

--- src/components/footer/Footer.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "./footer.css";
-
-const Footer = () => {
-    return <footer id="footer">Footer</footer>;
-};
-
-export default Footer;
--- src/components/testimoniales/Testimoniales.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "./testimoniales.css";
-
-const Testimoniales = () => {
-    return <section id="testimoniales">Testimoniales</section>;
-};
-
-export default Testimoniales;
