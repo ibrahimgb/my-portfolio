0e77743607d946ac1d9eb669e545ca174ae0dbfd
ibrahimgb <ibrahim.guoual.b@gmail.com>
2023-01-28 21:28:40 +0100

    set header to take full height

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -13,10 +13,9 @@ const Header = () => {
                 <h1 className="name">Ibrahim GUOUAL BELHAMIDI.</h1>{" "}
                 <h1 className="discription">I build things for the web.</h1>
                 <h5 className="text-light">
-                    I’m a software engineer student at Ahmed Ben Bella
-                    university passionate about IoT and embedded applications.
-                    Currently, I’m focused on building accessible mobile-first
-                    web apps. with clean scalable code.
+                I’m a passionate software engineer, self-learner, and highly motivated developer who loves solving problems, learning new skills!
+<br/>
+Passionate about IoT and embedded applications. Currently, I’m focused on building accessible mobile-first web apps. and aiming to use proven front-end and back-end skills to build fast,  clean scalable code, and maintainable software.
                 </h5>
                 <Btns />
                 <Socials />
